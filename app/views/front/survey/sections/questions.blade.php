af9ca441cd3d4d834176c8562d7495de9f44019a
Alex Foster <alex@aquanode.com>
2015-02-11 15:37:06 -0700

    New styles, sliders working..kinda, etc

--- app/views/front/survey/sections/questions.blade.php
+++ app/views/front/survey/sections/questions.blade.php
@@ -19,11 +19,14 @@
 						<div  class="" data-url="{{ url('api.survey.save') }}" data-value="1" data-question-id="{{ $question->id }}">
 						
 							
-								
-								
-									<div class="slider ui-slider ui-slider-handle ui-slider-horizontal "></div>
-
-							
+									
+									<div class="slider ui-slider ui-slider-handle ui-slider-horizontal ui-slider-range-max ui-slider-range-min">
+									<div>
+									<img src="/public/front/img/grey-circle.png" class="grey-circle" height="18px" width="18px" style="position: relative; top: -11px; left: -4px; z-index: 2;">
+									<img src="/public/front/img/orange-circle.png"  class="orange-circle" height="18px" width="18px" style="float: right; position: relative; top: -8px; right: -4px;">
+									</div>
+									</div>
+									
 
 							</div>
 
@@ -48,34 +51,56 @@
 
     $(".slider").slider({
         orientation: "horizontal",
-        range: false,
-        min: 5,
+        range: "min",
+        min: 0,
       	max: 100,
         value: 50,
-        step: .01,
+       step: 2 ,
         animate: true,
-        start: function(){
 
-        	$('.ui-slider-horizontal .ui-slider-handle:first').css({
-        		"background": " url('/public/front/img/boy-avatar.png')",
-        		padding: '18px', 
-        		top: "-15px"
+        slide: function(e){
+
+			$('.question-strength').find('.list-item').css({'background-color': 'transparent' , "color": '#777777'});
+
+        	$.each($('.slider'),function(){
+        		var position = $(this).slider("option", "value");
+
+        		if ($(this).find('.ui-slider-handle').css('background-image') == "url(http://meritcore.local/public/front/img/white-circle.png)") {
+	        		if (position >= 0 && position <= 20) {
+	        			$('#strongly-agree').css({"background-color": '#247fb9', "color": '#fff' });
+	        		}
+
+	        		if (position >= 20 && position <= 40) {
+	        			$('#agree').css({"background-color": '#247fb9', "color": '#fff' });
+	        		}
+        			if (position >= 40 && position <= 60) {
+	        			$('#not-sure').css({"background-color": '#247fb9', "color": '#fff' });
+	        		}
+	        	
+    				if (position >= 60 && position <= 80) {
+	        			$('#disagree').css({"background-color": '#247fb9', "color": '#fff' });
+	        		}
+					if (position >= 80 && position <= 100) {
+	        			$('#strongly-disagree').css({"background-color": '#247fb9', "color": '#fff' });
+	        		}
+        			
+        		}
+
+
+        	});
+        },
+
+       change: function(){
+
+        	$(this).find('.ui-slider-handle').css({
+        		"background": " url('/public/front/img/white-circle.png')",
+        		padding: '23px', 
+        		top: "-22px"
 
         	});
 
 
         }
-        ,
-   		slide: function(){
-   			$(this).add().css({ 
-
-		"background": " #fdb535"
-
-
-});
-
-
-   		}
 
         
     });
@@ -86,16 +111,25 @@ $('.ui-slider-horizontal').height(3);
 
 $('.ui-slider-horizontal .ui-slider-handle').css({
 	borderRadius: "80px" ,
-	top: "-27px",
+	top: "-28px",
 	opacity: '4',
 	border: "none",
 	padding: "28px" ,
-
+	"z-index": '4',
 	"background": "url('/public/front/img/default-circle.png') ",
 	"background-position": "center",
 
 });
 
+$('.slider .ui-slider-range').css({
+
+	'background': "#bfbfbf"
+
+});
+$('.ui-widget-content').css({
+'background':  "#fdb535"
+
+});
 
 </script>
 @stop
@@ -103,5 +137,5 @@ $('.ui-slider-horizontal .ui-slider-handle').css({
 
 @section('css')
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
-
+<link rel="stylesheet" href="/public/front/css/survey.css">
 @stop
