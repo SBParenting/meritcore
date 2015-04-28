0bcc9feaafac23a8a32dd86e063bebf3a6ef8fdd
Monark Bhagat <mb@example.com>
2015-04-28 09:12:09 -0600

    Heroes changed to ALL-CAPS

--- resources/views/front/survey/confirm.blade.php
+++ resources/views/front/survey/confirm.blade.php
@@ -18,7 +18,7 @@
 		        <div class="container">
 		            <div class="form-container">
 
-		               	<h3>Welcome to Heroes Student Survey</h3>
+		               	<h3>Welcome to HEROES® Student Survey</h3>
 		        		<p>To start the survey, please confirm your Name and Student ID.</p>
 		        		<hr />
 
