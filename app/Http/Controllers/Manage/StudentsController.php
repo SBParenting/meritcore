8ba14c9fed0df76fb029f4b05a6512d661840bdf
Joao Paulo Rabelo <joaoprabelo@gmail.com>
2015-05-14 14:26:31 -0600

    fix import assignment process

--- app/Http/Controllers/Manage/StudentsController.php
+++ app/Http/Controllers/Manage/StudentsController.php
@@ -235,7 +235,7 @@ class StudentsController extends Controller {
 			    	{
 			    		if ($field != 'skip')
 			    		{
-							$row_data[$key] = $row->{$field};
+							$row_data[$field] = $row->{$key};
 						}
 					}
 
